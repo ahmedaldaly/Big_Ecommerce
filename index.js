0424d9f6ed85ef64f60f6ce1573df30214a287c0
ahmedaldaly <ranaandahmed55@gmail.com>
2025-05-29 04:56:45 -0700

    securty

--- index.js
+++ index.js
@@ -3,12 +3,43 @@ const express = require('express');
 const dotenv = require('dotenv');
 const ConnectDB = require('./config/ConnectDB');
 const cors = require('cors')
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 // google
 const session = require("express-session");
 const passport = require("passport");
 const app = express();
 dotenv.config();
 ConnectDB();
+const generalLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute 
+  max:100 ,
+  message: 'لقد تجاوزت الحد المسموح للطلبات، حاول لاحقًا.',
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+//وضع حد للريكويست 
+app.use(generalLimiter);
+app.use(helmet()); //حماية السيرفر من هجمات  headers
+// حماية no sql يدوي
+app.use((req, res, next) => {
+  const sanitize = (obj) => {
+    for (let key in obj) {
+      if (key.startsWith('$') || key.includes('.')) {
+        delete obj[key];
+      } else if (typeof obj[key] === 'object') {
+        sanitize(obj[key]);
+      }
+    }
+  };
+
+  if (req.body) sanitize(req.body);
+  if (req.query) sanitize(req.query);
+  if (req.params) sanitize(req.params);
+
+  next();
+});
 
 app.use(cors());
 // google
